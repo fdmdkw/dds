4564af13ff03050c5c285c9469fa3bcac102961b
fdmdkw <lk1919618@gmail.com>
2019-01-13 18:38:40 +0800

    rasp compile config

--- examples/setup.py
+++ examples/setup.py
@@ -23,7 +23,11 @@ activate venv
 #step 1 compile ddsTable.cpp
 g++ -fPIC -O3 -flto -fopenmp -mtune=generic -std=c++11 -fopenmp -lboost_system -lboost_thread -DDDS_THREADS_BOOST -DDDS_THREADS_OPENMP -DDDS_THREADS_STL ddsTable.cpp -c ddsTable.o
 
+#raspberry pi
+g++ -fPIC -O3 -flto -fopenmp -Ofast -mtune=arm1176jzf-s -mfloat-abi=hard -march=armv6zk -std=c++11 -fopenmp -lboost_system -lboost_thread -DDDS_THREADS_BOOST -DDDS_THREADS_OPENMP -DDDS_THREADS_STL ddsTable.cpp -c ddsTable.o
+
 #step 2 linking files
+
 g++ ddsTable.o hands.o -shared -Wl,-O2 -Wl,--sort-common -Wl,--as-needed -Wl,-z -Wl,relro -fopenmp -lboost_system -lboost_thread -fPIC -L. -ldds -o libddsTable.so
 
 #step 3 copy shared library
